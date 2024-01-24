44de10c08dc71db63182249c39bf3645927b00f5
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-24 09:21:05 -0800

    adding mods to main js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@
 // ~15 hrs
 
 // MODS ADDED -------------------------------------------
-
+// - Reskinned: (3)
+//     - Rocket -> Nyan 
+//     - Spaceships -> poptarts
+//     - Explosion -> moving rainbow
+// - added random moving speeds for poptarts + nyan, depending on difficulty (2)
+// - added custom text font (3)
+// - added shot clock (3)
+// - Create a new title screen (e.g., new artwork, typography, layout) (3)
+// - added music (1)
+// - Create 4 new explosion sound effects and randomize which one plays on impact (3)
 // -------------------------------------------
 
 let config = {
